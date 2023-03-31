307a9da440d8524bfa7b81470d86d5c4993bf8c3
DiamondScythe <nhutoan399@gmail.com>
2023-03-31 07:26:48 +0700

    init vuetify

---
+++ src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <v-container>
+      <v-card>
+        <v-card-title> My Vuetify Component </v-card-title>
+        <v-card-text> This is a Vuetify component! </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style> -->
