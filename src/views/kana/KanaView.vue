08651926eff0f02ea8c46537d939e3e1458a2c51
DiamondScythe <nhutoan399@gmail.com>
2023-04-24 21:43:30 +0700

    added kana frontend

--- src/views/kana/KanaView.vue
+++ src/views/kana/KanaView.vue
@@ -1,3 +1,73 @@
-<template><div>this is kana view</div></template>
+<template>
+  <div>
+    <v-container class="card-container">
+      <v-card elevation="3" outlined shaped tile height="400px" width="1000px">
+        <v-row>
+          <v-col sm="12" lg="6" offset-lg="3">
+            <v-card-title class="justify-center"></v-card-title>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" lg="6" offset-lg="3">
+            <v-card-title class="justify-center">Kana practice</v-card-title>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" lg="6" offset-lg="3">
+            <v-card-title class="justify-center">あ</v-card-title>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" lg="6" offset-lg="3">
+            <v-text-field></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
+</template>
 
-<script></script>
+<script>
+import hiragana from "@/data/kana.js";
+import katakana from "@/data/kana.js";
+
+export default {
+  name: "KanaView",
+  components: {},
+  props: [],
+  data() {
+    return {
+      hiragana: hiragana,
+      katakana: katakana,
+    };
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style>
+.input {
+  text-align: center;
+}
+.text-center {
+  text-align: center;
+}
+.card-container {
+  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* .container {
+  border: 1px solid green;
+}
+.row {
+  border: 1px solid red;
+}
+.col {
+  border: 1px solid blue;
+} */
+</style>
