cc62a6345897799bed5f46c07cb771049e90800a
DiamondScythe <nhutoan399@gmail.com>
2023-04-24 11:49:33 +0700

    added due cards get for review page

--- src/views/review/ReviewView.vue
+++ src/views/review/ReviewView.vue
@@ -1,17 +1,41 @@
-<template></template>
+<template>
+  <div>
+    {{ flashcards }}
+  </div>
+</template>
 
 <script>
 import axios from "axios";
+import { checkAuthStatus } from "@/utils/auth";
 
 export default {
   name: "ReviewView",
   data() {
     return {
+      user: {},
       grammar: [],
+      flashcards: [],
     };
   },
-  mounted() {},
+  async mounted() {
+    const info = await checkAuthStatus();
+    if (info) {
+      this.user = info.user;
+      this.updateFlashcards();
+    }
+  },
   components: {},
-  methods: {},
+  methods: {
+    updateFlashcards() {
+      axios
+        .get("http://localhost:3000/api/dueFlashcards/" + this.user._id)
+        .then((res) => {
+          this.flashcards = res.data.flashcards;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
